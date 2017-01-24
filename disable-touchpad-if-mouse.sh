4e5b36cdc25cbb483963e81676fd220ef9a7a85b
Kevin Kellar <kellar.kevin@gmail.com>
2017-01-24 10:29:12 -0800

    Incresed checks

--- disable-touchpad-if-mouse.sh
+++ disable-touchpad-if-mouse.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
+
+if xinput | grep -q "Mouse";then
+bash $HOME/.scripts/pointerNT touchpad off
+fi
+sleep 1m
+if xinput | grep -q "Mouse";then
+bash $HOME/.scripts/pointerNT touchpad off
+fi
+sleep 3m
 if xinput | grep -q "Mouse";then
 bash $HOME/.scripts/pointerNT touchpad off
 fi
