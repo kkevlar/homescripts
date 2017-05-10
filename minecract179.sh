47e8e5dac579ff85abcf2df01d32c45a1ad743ee
Kevin Kellar <kellar.kevin@gmail.com>
2017-05-10 13:11:45 -0700

    used curl

--- minecract179.sh
+++ minecract179.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-wget -O SMC.jar "https://docs.google.com/uc?id=0B5_wYgbEk-GZOUY5X2lHVURTLUk&export=download"
+curl -o SMC.jar "https://docs.google.com/uc?id=0B5_wYgbEk-GZOUY5X2lHVURTLUk&export=download"
 
 chmod a+x SMC.jar
 
-perl gdown.pl 'https://docs.google.com/uc?id=0B5_wYgbEk-GZZURtbnJIeUFEU2M&export=download' 'mac-mc.zip'
+curl -L -o mac-mc.zip "https://github.com/rad-trophy/minecraft/releases/download/v1/minecraft-mac.zip"
 
 unzip mac-mc.zip -d $HOME/Library/Application\ Support/
 
-#https://drive.google.com/file/d/0B5_wYgbEk-GZZURtbnJIeUFEU2M/view?usp=sharing
 
-#https://docs.google.com/uc?id=0B5_wYgbEk-GZNmxfRmdmeWxUelE&export=download
-
-#https://docs.google.com/uc?export=download&confirm=R9EE&id=0B5_wYgbEk-GZNmxfRmdmeWxUelE
-
-#https://docs.google.com/uc?id=0B5_wYgbEk-GZZURtbnJIeUFEU2M&export=download
 
 
 
