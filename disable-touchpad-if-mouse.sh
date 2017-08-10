f10855a499792066a68c4b8c97bc6cbe6521e24e
Kevin Kellar <kellar.kevin@gmail.com>
2017-08-10 10:54:04 -0700

    cleaned up the homescripts

--- disable-touchpad-if-mouse.sh
+++ disable-touchpad-if-mouse.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 if xinput | grep -q "Mouse";then
-bash $HOME/.scripts/pointerNT touchpad off
+bash /home/kevlar/Documents/devt/homescripts/pointerNT touchpad off
 fi
-sleep 1m
-if xinput | grep -q "Mouse";then
-bash $HOME/.scripts/pointerNT touchpad off
+
+if xinput | grep -q "DeathAdder";then
+bash /home/kevlar/Documents/devt/homescripts/pointerNT touchpad off
 fi
-sleep 3m
-if xinput | grep -q "Mouse";then
-bash $HOME/.scripts/pointerNT touchpad off
-fi
+
--- mapstartup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xmodmap /home/kevlar/.scripts/rebindcaps
-
-
