aebc10477d8415b394bedc30d825a93e7e1156c8
Kevin Kellar <kellar.kevin@gmail.com>
2017-09-25 13:16:32 -0700

    addition to disable touchpad script

--- disable-touchpad-if-mouse.sh
+++ disable-touchpad-if-mouse.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 if xinput | grep -q "Mouse";then
-bash /home/kevlar/Documents/devt/homescripts/pointerNT touchpad off
+bash /home/kevlar/Documents/homescripts/pointerNT touchpad off
+fi
+
+if xinput | grep -q "Logitech M510";then
+bash /home/kevlar/Documents/homescripts/pointerNT touchpad off
 fi
 
 if xinput | grep -q "DeathAdder";then
-bash /home/kevlar/Documents/devt/homescripts/pointerNT touchpad off
+bash /home/kevlar/Documents/homescripts/pointerNT touchpad off
 fi
 
