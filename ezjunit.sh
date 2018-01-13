df54053efac2b5284108b97bcc59e83121709a23
Kevin Kellar <kellar.kevin@gmail.com>
2018-01-13 14:09:43 -0800

    final line in scritps

--- ezjunit.sh
+++ ezjunit.sh
@@ -2,4 +2,5 @@ mkdir -p ~/junitfiles
 wget -O ~/junitfiles/junit.jar http://www.csc.calpoly.edu/~phatalsk/public/jar/junit-4.12.jar
 wget -O ~/junitfiles/ham.jar http://www.csc.calpoly.edu/~phatalsk/public/jar/hamcrest-core-1.3.jar
 
+wget https://raw.githubusercontent.com/kkevlar/homescripts/master/jrun.sh
 
